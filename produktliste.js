6cb1b5d3168739e1d3dc3d1ec2b391b1534d12fa
magnus madsen <magn@stud.kea.dk>
2023-02-06 09:48:03 +0100

    nu med rigtig discount pris

--- produktliste.js
+++ produktliste.js
@@ -1,5 +1,5 @@
 async function hentData() {
-  const response = await fetch("https://kea-alt-del.dk/t7/api/products?limit=100");
+  const response = await fetch("https://kea-alt-del.dk/t7/api/products?limit=20");
   const data = await response.json();
   data.forEach(visProdukt);
 }
@@ -8,14 +8,15 @@ function visProdukt(produkt) {
   console.log(produkt);
   const template = document.querySelector("#prodcardtemplate").content;
   const klon = template.cloneNode(true);
-  let besparelse = produkt.discount / 100;
+  let besparelse = produkt.discount;
   let beforePrice = produkt.price;
+  let nyPris = (beforePrice * besparelse) / 100;
   klon.querySelector("h3").textContent = produkt.productdisplayname;
   klon.querySelector("h3").textContent = produkt.productdisplayname;
   klon.querySelector(".prodcard .prodstyle").textContent = produkt.articletype;
   klon.querySelector(".prodcard .prodbrand").textContent = produkt.brandname;
   klon.querySelector(".prodcard .realpris").textContent = produkt.price;
-  klon.querySelector(".prodcard .nupris").textContent = beforePrice * besparelse;
+  klon.querySelector(".prodcard .nupris").textContent = Math.floor(beforePrice - nyPris);
   klon.querySelector(".prodcard .proddiscount").textContent = produkt.discount;
 
   klon.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/1000/${produkt.id}.webp`;
