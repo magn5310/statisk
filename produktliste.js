ec1b7d204d6fab8b39cc0899bbf8cadb8a5a3ea1
magnus madsen <magn@stud.kea.dk>
2023-02-09 12:09:48 +0100

    dynamisk final

--- produktliste.js
+++ produktliste.js
@@ -1,23 +1,27 @@
+const urlParams = new URLSearchParams(window.location.search);
+let url = `https://kea-alt-del.dk/t7/api/products`;
+if (urlParams.get("cat")) {
+  const cat = urlParams.get("cat");
+  url = `https://kea-alt-del.dk/t7/api/products?category=${cat}&limit=14`;
+  document.querySelector("section h1").textContent = cat;
+}
 async function hentData() {
-  const response = await fetch("https://kea-alt-del.dk/t7/api/products?limit=20");
+  const response = await fetch(url);
   const data = await response.json();
   data.forEach(visProdukt);
 }
 hentData();
 function visProdukt(produkt) {
-  console.log(produkt);
   const template = document.querySelector("#prodcardtemplate").content;
   const klon = template.cloneNode(true);
-  let besparelse = produkt.discount;
-  let beforePrice = produkt.price;
-  let nyPris = (beforePrice * besparelse) / 100;
-  klon.querySelector("h3").textContent = produkt.productdisplayname;
+
   klon.querySelector("h3").textContent = produkt.productdisplayname;
   klon.querySelector(".prodcard .prodstyle").textContent = produkt.articletype;
   klon.querySelector(".prodcard .prodbrand").textContent = produkt.brandname;
   klon.querySelector(".prodcard .realpris").textContent = produkt.price;
-  klon.querySelector(".prodcard .nupris").textContent = Math.floor(beforePrice - nyPris);
+  klon.querySelector(".prodcard .nupris").textContent = Math.floor(produkt.price - (produkt.price * produkt.discount) / 100);
   klon.querySelector(".prodcard .proddiscount").textContent = produkt.discount;
+  klon.querySelector("a").href = "produkt.html?id=" + produkt.id;
 
   klon.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/1000/${produkt.id}.webp`;
   if (produkt.soldout) {
